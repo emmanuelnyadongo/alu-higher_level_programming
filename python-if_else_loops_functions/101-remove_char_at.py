213195284fe22d30941f1c1ac19109b1de9f73c5
emmanuelnyadongo <e.nyadongo@alustudent.com>
2023-02-10 10:50:34 -0800

    remove char at once

---
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def remove_char_at(str, n):
+    if n < 0 or n > len(str):
+        return str
+    elif n == len(str):
+        return str[:-1]
+    else:
+        return str[:n] + str[n + 1:]
