5f1c8ff50675929c4df580c5541a35ccac7be87e
emmanuelnyadongo <e.nyadongo@alustudent.com>
2023-02-10 04:07:54 -0800

    combined

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-for i in range (0, 89):
-    if(i // 10) < (i % 10):
-        print("{:02d}". format(i), end="")
-print(89)
+for x in range(10):
+    for y in range(x + 1, 10):
+        if x == 8 and y == 9:
+            print("{}{}".format(x, y))
+        else:
+            print("{}{}".format(x, y), end=", ")
