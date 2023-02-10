7c3340aefb5000499d97926dd1036f0585f2b85e
emmanuelnyadongo <e.nyadongo@alustudent.com>
2023-02-10 13:44:55 -0800

    magic calculations

--- python-if_else_loops_functions/102-magic_calculation.py
+++ python-if_else_loops_functions/102-magic_calculation.py
@@ -2,10 +2,9 @@
 
 
 def magic_calculation(a, b, c):
-    if not b < a:
-        if not c > b:
-            return a * b - c
-        else:
-            return a + b
-    else:
+    if a < b:
         return c
+    if c > b:
+        return a + b
+    else:
+        return a * b - c
