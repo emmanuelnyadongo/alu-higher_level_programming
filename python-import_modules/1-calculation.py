a683b266bf9995dcd68e95b243662e6c0af45eb4
emmanuelnyadongo <e.nyadongo@alustudent.com>
2023-03-07 08:32:16 -0800

    all calculations

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+
+
+def main():
+    a = 10
+    b = 5
+
+    # displayong calculating symbols
+    display(a, b, add(a, b), '+')
+    display(a, b, sub(a, b), '-')
+    display(a, b, mul(a, b), '*')
+    display(a, b, div(a, b), '/')
+
+
+def display(a, b, c, sign):
+    print("{} {} {} = {}".format(a, sign, b, c))
+
+
+if __name__ == "__main__":
+    main()
