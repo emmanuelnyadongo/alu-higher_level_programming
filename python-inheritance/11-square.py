d8d0502f687e2cb5d8330db64c6c2a402c015204
emmanuelnyadongo <e.nyadongo@alustudent.com>
2023-03-23 10:36:26 -0700

    11SQAURE

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
-"""Defines a Rectangle subclass Square."""
+"""
+Implements a square
+"""
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Represent a square."""
-
+    """
+    class Square - represents a square
+    methods:
+        - area() - returns the area
+    """
     def __init__(self, size):
-        """Initialize a new square.
-        Args:
-            size (int): The size of the new square.
-        """
-        self.integer_validator("size", size)
-        super().__init__(size, size)
+        super().integer_validator('size', size)
         self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    def __str__(self):
+        return f'[Square] {self.__size}/{self.__size}'
