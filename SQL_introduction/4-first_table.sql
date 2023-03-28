d9626ba156d84d4e66e2c6a2c0428527f0bf5c81
emmanuelnyadongo <e.nyadongo@alustudent.com>
2023-03-28 06:49:35 -0700

    creating table

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,3 +1,2 @@
--- creating a table with 2 columns.
-CREATE table IF NOT EXISTS first_table('id' INT, 'name' VARCHAR(256));
-
+-- creating a table in the current database
+CREATE table IF NOT EXISTS first_table(`id` INT, `name` VARCHAR(256));
