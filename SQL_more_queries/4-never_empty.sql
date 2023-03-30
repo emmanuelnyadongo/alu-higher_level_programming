cb6896c41abc8090fe356a663788d022ebb678b9
emmanuelnyadongo <e.nyadongo@alustudent.com>
2023-03-30 07:39:39 -0700

    changes

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,5 +1,5 @@
 -- creating a table with a id that has a not null value
 CREATE TABLE IF NOT EXISTS id_not_null(
-`id` INT NOT NULL DEFAULT VALUE 1,
+`id` INT NOT NULL DEFAULT  1,
 name VARCHAR(256)
 );
