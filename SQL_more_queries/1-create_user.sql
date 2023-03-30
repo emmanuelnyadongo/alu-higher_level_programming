519636ecc2ac77645e5d5761f51a3c6baaded36c
emmanuelnyadongo <e.nyadongo@alustudent.com>
2023-03-30 06:50:07 -0700

    database user

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- creating a new server user with all privilieges on the server
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd'
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'
