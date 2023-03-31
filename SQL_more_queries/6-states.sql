9120b42c7a843bb131c8b913602fa0f79a8bc43e
emmanuelnyadongo <e.nyadongo@alustudent.com>
2023-03-31 01:04:41 -0700

    changes

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- creating a database and a table with not null values
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states(
-`id` INT UNIQUE NOT NULL,
+`id` INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
 `name` VARCHAR(256) NOT NULL
 );
